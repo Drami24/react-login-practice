c384dd9f6e4958790503dece3be8bdcbe83834a0
damianld <damilores@gmail.com>
2020-09-22 16:14:27 +0200

    centrar formulario y aplicar estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import { LoginForm } from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="formulario">
-      <h1>Social Network</h1>
-      <LoginForm />
+    <div className="app-container">
+      <div className="formulario">
+        <h1>Social Network</h1>
+        <img
+          className="login-image"
+          src="https://render.fineartamerica.com/images/rendered/square-dynamic/small/images/artworkimages/mediumlarge/1/gandalf-tom-carlton.jpg"
+          //src="https://avatarfiles.alphacoders.com/209/thumb-209735.jpg"
+        />
+        <LoginForm />
+      </div>
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import PersonIcon from "@material-ui/icons/Person";
+import LockIcon from "@material-ui/icons/Lock";
 
 import "./LoginForm.scss";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,16 +23,40 @@ export const LoginForm = () => {
   const classes = useStyles();
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField name="email" label="Email" />
-      <TextField name="password" label="Password" />
-
-      <Button id="btn-login" variant="contained" color="primary">
-        Log in
-      </Button>
-      <Button id="btn-registry" color="primary">
-        Create account
-      </Button>
+    <form className="form-material" noValidate autoComplete="off">
+      <div className="login-input">
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <PersonIcon />
+          </Grid>
+          <Grid item>
+            <TextField name="email" label="Email" />
+          </Grid>
+        </Grid>
+      </div>
+      <div className="login-input">
+        <Grid container spacing={1} alignItems="flex-end">
+          <Grid item>
+            <LockIcon></LockIcon>
+          </Grid>
+          <Grid item>
+            <TextField name="password" type="password" label="Password" />
+          </Grid>
+        </Grid>
+      </div>
+      <div className="login-buttons">
+        <Button
+          id="btn-login"
+          className="login-button"
+          variant="contained"
+          color="primary"
+        >
+          Log in
+        </Button>
+        <Button className="login-button" id="btn-registry" color="primary">
+          Sign in
+        </Button>
+      </div>
     </form>
   );
 };
